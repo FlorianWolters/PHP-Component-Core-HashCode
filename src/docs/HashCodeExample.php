7bcd11dd0dbb1821ba683943ade45445ecfff16a
Florian Wolters <wolters.fl@gmail.com>
2013-02-22 12:03:50 +0100

    Updated example file. Signed-off-by:Florian Wolters <wolters.fl@gmail.com>

--- src/docs/HashCodeExample.php
+++ src/docs/HashCodeExample.php
@@ -1,52 +1,89 @@
 <?php
+namespace FlorianWolters;
+
 use FlorianWolters\Component\Core\HashCodeInterface;
+use FlorianWolters\Mock\HashCodeCustomImpl;
+use FlorianWolters\Mock\HashCodeDefaultImpl;
 
-require '../../vendor/autoload.php';
-require 'HashCodeDefaultImpl.php';
-require 'HashCodeCustomImpl.php';
+require __DIR__ . '/../../vendor/autoload.php';
 
-exit(main());
-
-function main()
+/**
+ * The class {@see HashCodeExample} implements a simple command line application
+ * to demonstrate the component **FlorianWolters\Component\Core\HashCode**.
+ *
+ * @author    Florian Wolters <wolters.fl@gmail.com>
+ * @copyright 2012-2013 Florian Wolters
+ * @license   http://gnu.org/licenses/lgpl.txt LGPL-3.0+
+ * @link      http://github.com/FlorianWolters/PHP-Component-Core-DebugPrint
+ * @since     Class available since Release 0.2.0
+ */
+final class HashCodeExample
 {
-    demonstrateDefaultImplementation();
-    demonstrateCustomImplementation();
+    /**
+     * Runs the {@see HashCodeExample}.
+     *
+     * @param integer $argc The number of arguments.
+     * @param array   $argv The arguments.
+     *
+     * @return integer Always `0`.
+     */
+    public static function main($argc, array $argv = array())
+    {
+        $self = new self;
+        $self->demonstrateDefaultImplementation();
+        $self->demonstrateCustomImplementation();
+
+        return 0;
+    }
+
+    /**
+     * @return void
+     */
+    private function demonstrateDefaultImplementation()
+    {
+        $firstObj = new HashCodeDefaultImpl;
+        $secondObj = new HashCodeDefaultImpl;
+        $thirdObj = new HashCodeDefaultImpl;
+
+        $this->writeLine('Default implementation:');
+        $this->output($firstObj, $secondObj, $thirdObj);
+    }
+
+    /**
+     * @return void
+     */
+    private function demonstrateCustomImplementation()
+    {
+        $firstObj = new HashCodeCustomImpl('foo');
+        $secondObj = new HashCodeCustomImpl('bar');
+        $thirdObj = new HashCodeCustomImpl('foo');
+
+        $this->writeLine('Custom implementation:');
+        $this->output($firstObj, $secondObj, $thirdObj);
+    }
+
+    /**
+     * @return void
+     */
+    private function output(
+        HashCodeInterface $firstObj,
+        HashCodeInterface $secondObj,
+        HashCodeInterface $thirdObj
+    ) {
+        $this->writeLine();
+        $this->writeLine('$firstObj->hashCode() = ' . $firstObj->hashCode());
+        $this->writeLine('$secondObj->hashCode() = ' . $secondObj->hashCode());
+        $this->writeLine('$thirdObj->hashCode() = ' . $thirdObj->hashCode());
+        $this->writeLine();
+    }
+
+    /**
+     * @return void
+     */
+    private function writeLine($text = '')
+    {
+        echo $text . \PHP_EOL;
+    }
 }
 
-function demonstrateDefaultImplementation()
-{
-    $firstObj = new HashCodeDefaultImpl;
-    $secondObj = new HashCodeDefaultImpl;
-    $thirdObj = new HashCodeDefaultImpl;
-
-    writeLine('Default implementation:');
-    output($firstObj, $secondObj, $thirdObj);
-}
-
-function output(
-    HashCodeInterface $firstObj,
-    HashCodeInterface $secondObj,
-    HashCodeInterface $thirdObj
-) {
-    writeLine();
-    // Member method
-    writeLine('$firstObj->hashCode() = ' . $firstObj->hashCode());
-    writeLine('$secondObj->hashCode() = ' . $secondObj->hashCode());
-    writeLine('$thirdObj->hashCode() = ' . $thirdObj->hashCode());
-    writeLine();
-}
-
-function writeLine($text = '')
-{
-    echo $text . \PHP_EOL;
-}
-
-function demonstrateCustomImplementation()
-{
-    $firstObj = new HashCodeCustomImpl('foo');
-    $secondObj = new HashCodeCustomImpl('bar');
-    $thirdObj = new HashCodeCustomImpl('foo');
-
-    writeLine('Custom implementation:');
-    output($firstObj, $secondObj, $thirdObj);
-}
+exit(HashCodeExample::main($argc, $argv));
